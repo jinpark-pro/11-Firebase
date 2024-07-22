bec1e30346b244edd3277815c6b239899f0f9080
Jin Park <jinpark1504@gmail.com>
2024-07-22 13:36:26 +0900

    fetch data from firebase

--- NoteApp/NoteApp/ContentView.swift
+++ NoteApp/NoteApp/ContentView.swift
@@ -11,11 +11,22 @@ struct ContentView: View {
     @State private var showSheet = false
     @State private var postDetent = PresentationDetent.medium
     
+    @StateObject private var viewModel = NoteViewModel()
+    
     var body: some View {
         NavigationStack {
             List {
-                
+                ForEach(viewModel.notes, id: \.id) { note in
+                    VStack(alignment: .leading) {
+                        Text(note.title ?? "")
+                            .font(.system(size: 22, weight: .regular))
+                    }
+                }
             }
+            .onAppear {
+                viewModel.fetchData()
+            }
+            .navigationTitle("Notes")
             .toolbar {
                 ToolbarItemGroup(placement: .bottomBar) {
                     Text(" X notes")
--- NoteApp/NoteApp/NoteViewModel.swift
+++ NoteApp/NoteApp/NoteViewModel.swift
@@ -17,4 +17,18 @@ class NoteViewModel: ObservableObject {
         let docRef = databaseReference.addDocument(data: ["title": title])
         dump(docRef)
     }
+    
+    func fetchData() {
+        // snapshotlistener: firestore 에서 지속적으로 메모리에 떠있으면서 변화 관찰
+        databaseReference.addSnapshotListener { (querySnapshot, erro) in
+            guard let documents = querySnapshot?.documents else {
+                print("No documents")
+                return
+            }
+            // nosql 은 (key: value) 로 값이 없을수도 있으므로 optional
+            self.notes = documents.compactMap { docSnap -> Note? in
+                return try? docSnap.data(as: Note.self)
+            }
+        }
+    }
 }
