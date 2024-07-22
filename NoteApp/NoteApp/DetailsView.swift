4ebb1a4180f4d28ae5722459632e486d76e3c8b7
Jin Park <jinpark1504@gmail.com>
2024-07-22 15:21:02 +0900

    Use EnvironmentObject

--- NoteApp/NoteApp/DetailsView.swift
+++ NoteApp/NoteApp/DetailsView.swift
@@ -13,7 +13,7 @@ struct DetailsView: View {
     @State private var presentAlert = false
     @State private var titleText = ""
     
-    @ObservedObject var viewModel: NoteViewModel
+    @EnvironmentObject var viewModel: NoteViewModel
     
     var body: some View {
         NavigationStack {
@@ -55,5 +55,6 @@ struct DetailsView: View {
 }
 
 #Preview {
-    DetailsView(note: Note(title: "Test"), viewModel: NoteViewModel())
+    DetailsView(note: Note(title: "Test"))
+        .environmentObject(NoteViewModel())
 }
