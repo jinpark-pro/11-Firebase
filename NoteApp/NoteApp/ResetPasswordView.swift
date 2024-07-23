5e040c0a6dd24f8d67498a268c8342e54b11c8a0
Jin Park <jinpark1504@gmail.com>
2024-07-23 11:02:09 +0900

    Add SignUpView

---
+++ NoteApp/NoteApp/ResetPasswordView.swift
@@ -0,0 +1,18 @@
+//
+//  ResetPasswordView.swift
+//  NoteApp
+//
+//  Created by Jungjin Park on 2024-07-23.
+//
+
+import SwiftUI
+
+struct ResetPasswordView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ResetPasswordView()
+}
