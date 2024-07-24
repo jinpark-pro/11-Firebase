9a6511eaa1fd1859df58084dea27b0ea28412ddb
Jin Park <jinpark1504@gmail.com>
2024-07-24 11:54:19 +0900

    Add image to user profile

--- Socially/Socially/Accounts/ViewModels/AuthViewModel.swift
+++ Socially/Socially/Accounts/ViewModels/AuthViewModel.swift
@@ -7,6 +7,8 @@
 
 import SwiftUI
 import FirebaseAuth
+import FirebaseFirestore
+import FirebaseStorage
 import AuthenticationServices
 import CryptoKit
 
@@ -28,7 +30,85 @@ class AuthViewModel: ObservableObject {
         }
     }
     
-    func randomNonceString(length: Int = 32) -> String {
+    // MARK: - Profile Image
+    func uploadProfileImage(_ imageData: Data) {
+        let storageReference = Storage.storage().reference().child("\(UUID().uuidString)")
+        
+        storageReference.putData(imageData, metadata: nil) { metadata, error in
+            if let error = error {
+                return
+            }
+            
+            storageReference.downloadURL { url, error in
+                if let imageURL = url,
+                   let user = Auth.auth().currentUser {
+                    let changeRequest = user.createProfileChangeRequest()
+                    changeRequest.photoURL = imageURL
+                    changeRequest.commitChanges {
+                        error in
+                        if let error = error {
+                            print("\(error.localizedDescription)")
+                            return
+                        }
+                        self.user = Auth.auth().currentUser
+                    }
+                }
+            }
+        }
+    }
+    
+    // MARK: - Sign in with Apple Methods
+    func signInWithApple(request: ASAuthorizationAppleIDRequest) {
+        let nonce = randomNonceString()
+        currentNonce = nonce
+        request.requestedScopes = [.email]
+        request.nonce = sha256(nonce)
+    }
+    
+    func signInWithAppleCompletion(result: Result<ASAuthorization, any Error>) {
+        switch result {
+        case .success(let authResults):
+            switch authResults.credential {
+            case let appleIDCredential as ASAuthorizationAppleIDCredential:
+                guard let nonce = currentNonce else {
+                    fatalError("Invalid state: A login callback was received, but no login request was sent.")
+                }
+                guard let appleIDToken = appleIDCredential.identityToken else {
+                    fatalError("Invalid state: A login callback was received, but no login request was sent.")
+                }
+                guard let idTokenString = String(data: appleIDToken, encoding: .utf8) else {
+                    print("Unable to serialize token string from data: \(appleIDToken.debugDescription)")
+                    return
+                }
+                let credential = OAuthProvider.credential(withProviderID: "apple.com",
+                                                          idToken: idTokenString,
+                                                          rawNonce: nonce)
+                Auth.auth().signIn(with: credential) { (authResult, error) in
+                    if let error = error {
+                        print(error.localizedDescription)
+                        return
+                    }
+                    print("signed in")
+                    guard let user = authResult?.user else { return }
+                    
+                    let userData = ["email": user.email, "uid": user.uid]
+                    Firestore.firestore().collection("User")
+                        .document(user.uid)
+                        .setData(userData as [String : Any]) { _ in
+                            print("DEBUG: Did upload user data.")
+                        }
+                }
+                print("\(String(describing: Auth.auth().currentUser?.uid))")
+            default:
+                break
+            }
+        case .failure(let error):
+            print(error.localizedDescription)
+        }
+    }
+    
+    // MARK: - Private Methods
+    private func randomNonceString(length: Int = 32) -> String {
         precondition(length > 0)
         let charset: [Character] =
             Array("0123456789ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvwxyz-._")
@@ -62,7 +142,7 @@ class AuthViewModel: ObservableObject {
         return result
     }
 
-    func sha256(_ input: String) -> String {
+    private func sha256(_ input: String) -> String {
         let inputData = Data(input.utf8)
         let hashedData = SHA256.hash(data: inputData)
         let hashString = hashedData.compactMap {
