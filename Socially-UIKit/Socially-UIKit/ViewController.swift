9afe3dcb3a30812de3cefc12eefab1b75c51f153
Jin Park <jinpark1504@gmail.com>
2024-07-25 09:41:23 +0900

    initial uikit

---
+++ Socially-UIKit/Socially-UIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Socially-UIKit
+//
+//  Created by Jungjin Park on 2024-07-25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
