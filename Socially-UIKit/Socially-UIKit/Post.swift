c15a0d7a8ca66df63b1d29530be8ccf3ab885742
Jin Park <jinpark1504@gmail.com>
2024-07-25 11:35:11 +0900

    Set ViewController Title and TabBar

---
+++ Socially-UIKit/Socially-UIKit/Post.swift
@@ -0,0 +1,16 @@
+//
+//  Post.swift
+//  Socially-UIKit
+//
+//  Created by Jungjin Park on 2024-07-25.
+//
+
+import Foundation
+import FirebaseFirestore
+
+struct Post: Identifiable, Decodable {
+    @DocumentID var id: String?
+    var description: String?
+    var imageURL: String?
+    @ServerTimestamp var datePublished: Date?
+}
