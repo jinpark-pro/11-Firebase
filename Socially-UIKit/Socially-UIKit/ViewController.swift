4739adaec69cd6e6fc27919194da80a842ef00fa
Jin Park <jinpark1504@gmail.com>
2024-07-25 11:44:36 +0900

    Initialize tableView and diffableDataSource

--- Socially-UIKit/Socially-UIKit/ViewController.swift
+++ Socially-UIKit/Socially-UIKit/ViewController.swift
@@ -6,13 +6,39 @@
 //
 
 import UIKit
+import FirebaseFirestore
 
 class ViewController: UIViewController {
-
+    enum Section {
+        case main
+    }
+    
+    private var db: Firestore!
+    private var dataSource: UITableViewDiffableDataSource<Section, Post>!
+    private var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = "Feed"
         self.navigationController?.navigationBar.prefersLargeTitles = true
         self.tabBarItem = UITabBarItem(title: "Feed", image: UIImage(systemName: "text.bubble"), tag: 0)
+    }
+    
+    func configureTableView() {
+        tableView = UITableView(frame: view.bounds, style: .plain)
+        view.addSubview(tableView)
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "postCell")
+        tableView.rowHeight = 280
+    }
+    
+    func configureDataSource() {
+        dataSource = UITableViewDiffableDataSource<Section, Post>(tableView: tableView) { (tableView, indexPath, item) -> UITableViewCell? in
+            let cell = tableView.dequeueReusableCell(withIdentifier: "postCell")
+            var config = cell?.defaultContentConfiguration()
+            config?.text = item.description
+            cell?.contentConfiguration = config
+            
+            return cell
+        }
     }
 }
